9d18862bb8faf3d440bfd782ffc40dc9b69e9417
sosaskze143 <sosaskze143@gmail.com>
2025-02-25 21:48:04 +0300

    Create script.js

---
+++ public/js/script.js
@@ -0,0 +1,11 @@
+document.getElementById('new-request').addEventListener('click', () => {
+    alert('تم النقر على "رفع طلب جديد"');
+});
+
+document.getElementById('my-requests').addEventListener('click', () => {
+    alert('تم النقر على "طلباتي"');
+});
+
+document.getElementById('support').addEventListener('click', () => {
+    alert('تم النقر على "الدعم الفني"');
+});
